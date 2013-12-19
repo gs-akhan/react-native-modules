eacfb1daef5e432f2f60efe8a2522f3202ce0eed
gs-akhan <akhan@gainsight.com>
2013-12-19 19:32:10 -0400

    Removed some code

--- index.ios.js
+++ index.ios.js
@@ -15,18 +15,14 @@ var {
 var HelloApp = React.createClass({
   render: function() {
     return (
+
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        
       </View>
+
     );
   }
 });
