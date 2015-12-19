701060fdbc75f8c39b5db74dd09c800f76255c60
gs-akhan <akhan@gainsight.com>
2015-12-19 19:32:10 -0400

    added some code

--- index.ios.js
+++ index.ios.js
@@ -20,9 +20,15 @@ var HelloApp = React.createClass({
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
-        
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
       </View>
-
+      
     );
   }
 });
